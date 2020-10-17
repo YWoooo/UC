e1bccbe62ebf98983c6a5f954374ee263d6f321c
YuWeiChang <ywchangsmail@gmail.com>
2020-10-18 03:41:02 +0800

    fix: can't go to the right page by DefaultBottomNav.vue - Seems like using router name to router.push may casue some problem in Nuxt. - Using path for now.

--- components/layouts/DefaultBottomNav.vue
+++ components/layouts/DefaultBottomNav.vue
@@ -3,41 +3,39 @@
     <v-btn
       v-for="item in btnDatas"
       :key="item.label"
-      :value="item.routeName"
-      @click="goTo(item.routeName)"
+      @click="goTo(item.url)"
     >
       <span>{{ item.label }}</span>
       <v-icon>{{ item.icon }}</v-icon>
     </v-btn>
   </v-bottom-navigation>
 </template>
+
 <script lang='ts'>
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+
 @Component
 export default class DefaultBottomNav extends Vue {
   public btnDatas = [
     {
       label: "Home",
       icon: "mdi-home",
-      routeName: "home",
+      path: "/home",
     },
     {
       label: "Wallet",
       icon: "mdi-cash-usd",
-      routeName: "wallet",
+      path: "/wallet",
     },
     {
       label: "Settings",
       icon: "mdi-cog-outline",
-      routeName: "settings",
+      path: "/settings",
     },
   ];
-  public goTo(routeName: string) {
-    this.$router.push(routeName);
+  public goTo(url: string) {
+    this.$router.push(url);
   }
 }
 </script>
-<style lang="scss" scoped>
-@import '~/assets/styles/index.scss';
-</style>
