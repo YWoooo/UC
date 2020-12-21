07e2400d47002a276db8c119dab2f051bbb32eb4
YuWeiChang <ywchangsmail@gmail.com>
2020-12-21 21:51:34 +0800

    Fix: add new TheMessage for notLogin.vue.

--- layouts/notLogin.vue
+++ layouts/notLogin.vue
@@ -4,7 +4,13 @@
       <NotLoginNav />
       <nuxt />
     </div>
-    <TheMessage />
+    <v-slide-y-transition :group="true" >
+      <TheMessage 
+        v-for="(msg, index) in msgs" 
+        :key="index"
+        :msg="msg"
+        transition="slide-x-transition" />           
+    </v-slide-y-transition>
   </v-app>
 </template>
 
@@ -12,7 +18,12 @@
 import { Component, Vue } from "nuxt-property-decorator";
 import NotLoginNav from "@/components/layouts/NotLoginNav.vue";
 import TheMessage from "@/components/global/the-message/index.vue";
+import { theMessageStore } from "~/store";
 
 @Component({ components: { NotLoginNav, TheMessage } })
-export default class notLogin extends Vue {}
+export default class notLogin extends Vue {
+  public get msgs() {
+    return theMessageStore.msgs
+  }
+}
 </script>
