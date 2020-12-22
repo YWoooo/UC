9a7b1b16b1d641b09a848159dd3b05756f81d240
YuWeiChang <ywchangsmail@gmail.com>
2020-12-22 22:16:55 +0800

    Feat: errMsg.ts.

---
+++ request/configs/errMsgs.ts
@@ -0,0 +1,19 @@
+export const errMsggs: { [key: number]: string } = {
+  0: 'System error, please contact us.',
+  400001: 'Sorry, but the user doesn\'t exist.',
+  400002: 'Sorry, but there\'s no enough balance in your account.',
+  400003: 'Sorry, but the rate is incorrect.',
+  400004: 'Sorry, but the amount is over or under limit.',
+  400005: 'Please check the amount again.',
+  400006: 'Sorry, but please apply within 9:00 to 18:00.',
+}
+
+// export enum resCode {
+//   missingParams = 400000,
+//   notExistUser = 400001,
+//   notEnoughBalance = 400002,
+//   incorrectRate = 400003,
+//   overOrUnderLimit = 400004,
+//   incorrectAmount = 400005,
+//   notOfficeHour = 400006,
+// }
