91e344edc43efddbe7ed485516ebd6b4cbdea769
YuWeiChang <ywchangsmail@gmail.com>
2021-01-11 21:48:06 +0800

    Feat: onResponse & onResponseError of axios.

--- request/axios.ts
+++ request/axios.ts
@@ -1,7 +1,10 @@
 import { Context } from '@nuxt/types'
-import { AxiosRequestConfig } from '@nuxtjs/axios/node_modules/axios'
+import { AxiosRequestConfig, AxiosResponse, AxiosError } from '@nuxtjs/axios/node_modules/axios'
 import { onRequest } from './interceptors/onRequest'
+import { onResponseSuccess, onResponseErr } from './interceptors/onResponse'
 
 export default function ({ $axios }: Context) {
   $axios.onRequest((config: AxiosRequestConfig) => onRequest(config))
+  $axios.onResponse((res: AxiosResponse) => onResponseSuccess(res))
+  $axios.onResponseError((err: AxiosError) => onResponseErr(err))
 }
