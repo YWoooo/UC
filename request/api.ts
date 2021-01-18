c59baa4d7f6c80181284a1b3cf291ed140b465a8
YuWeiChang <ywchangsmail@gmail.com>
2021-01-18 21:27:34 +0800

    Feat: use cookie-universal-nuxt for onRequest.

--- request/api.ts
+++ request/api.ts
@@ -1,6 +1,6 @@
 import { Plugin } from '@nuxt/types'
 import { AxiosRequestConfig, AxiosResponse, AxiosError } from '@nuxtjs/axios/node_modules/axios'
-import { onRequest } from './interceptors/onRequest'
+import { onRequest, CookiesAboutRequest } from './interceptors/onRequest'
 import { onResponseSuccess, onResponseErr } from './interceptors/onResponse'
 
 declare module 'vue/types/vue' {
@@ -21,7 +21,7 @@ declare module 'vuex/types/index' {
   }
 }
 
-export const api: Plugin = ({ $axios }, inject) => {
+export const api: Plugin = ({ $axios, app }, inject) => {
   /**
    * Seems like there's some type bugs about $axios.create, 
    * which's not fixed since Sep 24, 2020, 
@@ -29,18 +29,34 @@ export const api: Plugin = ({ $axios }, inject) => {
    * See https://github.com/nuxt/typescript/issues/447
    * Last time checked: Jan 11, 2021.
    */
-  const api: any = $axios.create({
+  const axios: any = $axios.create({
     headers: {
       common: {
         Accept: 'text/plain, */*'
       }
     }
   })
+  const cookies: CookiesAboutRequest = {
+    accessToken: app.$cookies.get('accessToken'),
+    refreshTime: app.$cookies.get('refreshTime'),
+    refreshToken: app.$cookies.get('refreshToken')
+  }
 
-  api.setBaseURL('http://localhost:3001')
-  api.onRequest((config: AxiosRequestConfig) => onRequest(config))
-  api.onResponse((res: AxiosResponse) => onResponseSuccess(res))
-  api.onResponseError((err: AxiosError) => onResponseErr(err))
+  axios.setBaseURL('http://localhost:3001')
+  axios.onRequest((config: AxiosRequestConfig) => onRequest(config, cookies))
+  axios.onResponse((res: AxiosResponse) => onResponseSuccess(res))
+  axios.onResponseError((err: AxiosError) => onResponseErr(err))
+
+  const api = {
+    async login(sendData: any) {
+      const res = await axios.$post('/login', sendData)
+      return res
+    },
+    async getUserInfo() {
+      const res = await axios.$get('/userInfo')
+      return res
+    },
+  }
 
   // Inject to context as $api
   inject('api', api)
--- request/interceptors/onRequest.ts
+++ request/interceptors/onRequest.ts
@@ -1,28 +1,28 @@
 import { AxiosRequestConfig } from '@nuxtjs/axios/node_modules/axios'
-import cookiejs from 'cookiejs'
 
-export const onRequest = (reqConfig: AxiosRequestConfig) => {
-  setAccessToken(reqConfig)
-  setRefreshToken(reqConfig)
+export interface CookiesAboutRequest {
+  accessToken: string | undefined
+  refreshTime: string | undefined
+  refreshToken: string | undefined
+}
+
+export const onRequest = (reqConfig: AxiosRequestConfig, cookies: CookiesAboutRequest) => {
+  if (cookies.accessToken) {
+    setAccessToken(reqConfig, cookies.accessToken)
+  }
+  if (cookies.refreshTime && cookies.refreshToken) {
+    setRefreshToken(reqConfig, cookies.refreshTime, cookies.refreshToken)
+  }
   return reqConfig
 }
 
-const setAccessToken = (reqConfig: AxiosRequestConfig) => {
-  const accessToken: any = cookiejs.get('accessToken')
-  if (accessToken) {
-    reqConfig.headers.Authorization = 'Bearer ' + accessToken
-  }
+const setAccessToken = (reqConfig: AxiosRequestConfig, accessToken: string) => {
+  reqConfig.headers.Authorization = 'Bearer ' + accessToken
 }
 
-const setRefreshToken = (reqConfig: AxiosRequestConfig) => {
-  const refreshTime: any = cookiejs.get('refreshTime')
-  const isOver10Mins = refreshTime && Math.floor(Date.now() / 1000) - refreshTime >= 600
-
+const setRefreshToken = (reqConfig: AxiosRequestConfig, refreshTime: string, refreshToken: string) => {
+  const isOver10Mins = Math.floor(Date.now() / 1000) - +refreshTime >= 600
   if (isOver10Mins) {
-    const refreshToken: any = cookiejs.get('refreshToken')
-
-    if (refreshToken) {
-      reqConfig.headers.RefreshToken = refreshToken
-    }
+    reqConfig.headers.RefreshToken = refreshToken
   }
 }
