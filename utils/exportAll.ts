1a6f95a4917f36e347755491400c1f7b0ed9e1ba
YuWeiChang <ywchangsmail@gmail.com>
2021-01-26 21:32:23 +0800

    Feat: add RequireContext interface for exportAll.ts - Copied from '@types/webpack-env/index.d.ts', - since it doesn't have exporting parts.

--- utils/exportAll.ts
+++ utils/exportAll.ts
@@ -5,7 +5,7 @@
  * @example exportAll(require.context('./', false, /\.ts$/)) 
  */
 type Files = any
-export default function (files: any): Files {
+export default function (files: RequireContext): Files {
   const res: any = {}
 
   files.keys().forEach((fileName: string) => {
@@ -15,4 +15,14 @@ export default function (files: any): Files {
     res[serviceName] = files(fileName).default
   })
   return res
+}
+
+// Copied from '@types/webpack-env/index.d.ts', since it doesn't have exporting parts.
+interface RequireContext {
+  keys(): string[];
+  (id: string): any;
+  <T>(id: string): T;
+  resolve(id: string): string;
+  /** The module id of the context module. This may be useful for module.hot.accept. */
+  id: string;
 }
