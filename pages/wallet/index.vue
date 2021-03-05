b0c0a621b413b05442f62468a9ee6de6933246c2
YuWeiChang <ywchangsmail@gmail.com>
2021-03-06 00:37:32 +0800

    Feat (wallet): getUserInfo api connected.

--- pages/wallet/index.vue
+++ pages/wallet/index.vue
@@ -2,20 +2,28 @@
   <div class="wallet">
     <div>
       <WalletOverview />
-      <WalletMenu />
+      <WalletMenu />      
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
+import { Context } from '@nuxt/types'
 import WalletOverview from "@/components/wallet/WalletOverview.vue";
 import WalletMenu from "@/components/wallet/WalletMenu.vue";
+
 @Component({
   components: { WalletOverview, WalletMenu },
 })
-export default class Wallet extends Vue {}
+export default class Wallet extends Vue {
+  public async asyncData({ app }: Context) {
+    const user = await app.$api.getUserInfo()
+    return { user }
+  }
+}
 </script>
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 @import "~/assets/styles/index.scss";
 .wallet {
   display: flex;
