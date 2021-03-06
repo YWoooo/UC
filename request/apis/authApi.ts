eb42cd9148f61c8adc48ee86e4c788ec7b05eed8
YuWeiChang <ywchangsmail@gmail.com>
2021-03-06 19:53:49 +0800

    Refactor (authApi): type the sendData of login.

--- request/apis/authApi.ts
+++ request/apis/authApi.ts
@@ -1,6 +1,8 @@
+import { Login } from '@/interfaces/login'
+
 export default function authApi(axios: any) {
   return {
-    async login(sendData: any) {
+    async login(sendData: Login.SendData) {
       const res = await axios.$post('/login', sendData)
       return res
     }
