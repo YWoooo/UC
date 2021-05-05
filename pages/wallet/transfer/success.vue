da2e4c71e019f4171b73325f9c1571078d1eae79
YuWeiChang <ywchangsmail@gmail.com>
2021-05-05 23:26:23 +0800

    feat (transfer): new success page.

--- pages/wallet/transfer/success.vue
+++ pages/wallet/transfer/success.vue
@@ -4,26 +4,32 @@
 </template>
 
 <script lang='ts'>
-import { Component, Vue, Prop } from "nuxt-property-decorator";
-import TheSuccessPage from '@/components/layouts/the-success-page/index.vue';
-import { SuccessPageConfig } from '@/interfaces/TheSuccessPage';
+import { Component } from "nuxt-property-decorator";
+import PaySuccess from '../PaySuccess'
 
-@Component({ components: { TheSuccessPage }})
-// TODO: access.
-export default class TransferSuccess extends Vue {
-  public successPageConfig: SuccessPageConfig =  {
-    mdiIcon: 'transfer',
-    title: 'Transfer success',
-    subtitle1: 'You\'ve withdrawaled 6000 USD ',
-    subtitle2: 'form ****** to ******',
-    btnPri: {
-      text: 'To Wallet',
-      onClick: () => this.$router.push('/wallet') 
-    },
-    btnSub: {
-      text: 'To Home',
-      onClick: () => this.$router.push('/') 
+@Component
+export default class TransferSuccess extends PaySuccess {
+  public get successPageConfig() {
+    return {
+      mdiIcon: 'transfer',
+      title: 'Transfer success.',
+      subtitle1: `You\'ve transfer ${this.toAmountText} USD `,
+      subtitle2: `from ${this.fromAccount} to ${this.toAccount}`,
+      btnPri: {
+        text: 'To Wallet',
+        onClick: () => this.$router.push('/wallet') 
+      },
+      btnSub: {
+        text: 'To Home',
+        onClick: () => this.$router.push('/') 
+      }
     }
+  }
+  public get fromAccount() {
+    return this.$route.query.f || ''
+  }
+  public get toAccount() {
+    return this.$route.query.t || ''
   }
 }
 </script>
