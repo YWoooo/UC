51b096e92aa9454e863119a7ec6fe3a01d9122d9
YuWeiChang <ywchangsmail@gmail.com>
2021-05-08 04:24:42 +0800

    Feat: put all the wallet configs ina single file.

---
+++ configs/wallet.ts
@@ -0,0 +1,17 @@
+export const depositConfigs = {
+  minAmount: 100,
+  maxAmount: 10000
+}
+export const withdrawalConfigs = {
+  minAmount: 100,
+  maxAmount: 10000
+}
+export const transferConfigs = {
+  minAmount: 100,
+  maxAmount: 10000
+}
+export default {
+  deposit: depositConfigs,
+  withdrawal: withdrawalConfigs,
+  transfer: transferConfigs
+}
